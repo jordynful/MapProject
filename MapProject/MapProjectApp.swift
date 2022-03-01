3ee20bfa2c6130adb68ab9be757fac17d64692f7
Jordyn Fulbright <jordynfulbright@gmail.com>
2022-03-01 10:08:19 -0500

    Initial Commit

---
+++ MapProject/MapProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapProjectApp.swift
+//  MapProject
+//
+//  Created by Jordyn Fulbright on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MapProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
